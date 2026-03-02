b436b474cb6766c154c95e7c46f6602e5bec0b33
邱榮誠 <a0909588693@gmail.com>
2026-03-02 21:24:28 +0800

    Add files via upload

--- Lab1_Integer MultiplyDivide Unit/lab1/lab1/imuldiv/imuldiv.mk
+++ Lab1_Integer MultiplyDivide Unit/lab1/lab1/imuldiv/imuldiv.mk
@@ -1,1 +1,28 @@
+#=========================================================================
+# imuldiv Subpackage
+#=========================================================================
 
+imuldiv_deps = vc
+
+imuldiv_srcs = \
+  imuldiv-IntMulIterative.v \
+  imuldiv-IntDivIterative.v \
+  imuldiv-IntMulDivIterative.v \
+  imuldiv-IntMulDivSingleCycle.v \
+  imuldiv-IntMulBooth.v \
+  imuldiv-IntMulThreeInput.v \
+  imuldiv-ThreeMulReqMsg.v \
+
+imuldiv_test_srcs = \
+  imuldiv-IntMulIterative.t.v \
+  imuldiv-IntDivIterative.t.v \
+  imuldiv-IntMulDivIterative.t.v \
+  imuldiv-IntMulDivSingleCycle.t.v \
+  imuldiv-IntMulBooth.t.v \
+  imuldiv-IntMulThreeInput.t.v \
+  imuldiv-ThreeMulReqMsg.t.v \
+  
+imuldiv_prog_srcs = \
+  imuldiv-singcyc-sim.v \
+  imuldiv-iterative-sim.v \
+  imuldiv-booth-sim.v \
