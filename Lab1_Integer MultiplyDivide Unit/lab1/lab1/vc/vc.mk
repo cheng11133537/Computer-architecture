99d19559fe06d71aebd0e02ed3abc813089486b7
邱榮誠 <a0909588693@gmail.com>
2026-03-02 21:26:07 +0800

    Add files via upload

--- Lab1_Integer MultiplyDivide Unit/lab1/lab1/vc/vc.mk
+++ Lab1_Integer MultiplyDivide Unit/lab1/lab1/vc/vc.mk
@@ -1,1 +1,30 @@
+#=========================================================================
+# vc Subpackage
+#=========================================================================
+
+vc_deps =
+
+vc_srcs = \
+  vc-Test.v \
+  vc-TestSource.v \
+  vc-TestSink.v \
+  vc-Misc.v \
+  vc-Muxes.v \
+  vc-StateElements.v \
+  vc-Regfiles.v \
+  vc-RAMs.v \
+  vc-Queues.v \
+  vc-Memories.v \
+  vc-Trace.v \
+
+vc_test_srcs = \
+  vc-Misc.t.v \
+  vc-Muxes.t.v \
+  vc-StateElements.t.v \
+  vc-Regfiles.t.v \
+  vc-RAMs.t.v \
+  vc-Queues.t.v \
+  vc-Memories.t.v \
+
+vc_prog_srcs = \
 
